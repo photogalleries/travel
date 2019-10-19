5f48717071666cc38188836848ae3c78f4641617
Ryan Modrak <rmodrak@gmail.com>
2019-10-19 12:02:56 -0600

    Initial commit

---
+++ build.bash
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+
+LOCAL="$HOME/web/photogalleries/travel"
+REMOTE="photogalleries/travel"
+
+SRC='/Volumes/Data/Pictures'
+DST='photos'
+
+
+cd $LOCAL
+setup
+
+ln -s $SRC/places/* $DST
+ln -s "$SRC/pictures-3/Geology, U.S./" "$DST/U.S. Geology"
+ln -s "$SRC/pictures-4/0001/" "$DST/U.S. Cities"
+
+build $LOCAL $REMOTE
+
+
