cd3156f7e28b8dea3ede93303b5763614a0737d7
Ryan Modrak <rmodrak@gmail.com>
2019-10-25 20:04:48 -0600

    Updates

--- build.bash
+++ build.bash
@@ -12,7 +12,7 @@ cd $LOCAL
 setup
 
 ln -s $SRC/places/* $DST
-ln -s "$SRC/pictures-3/Geology, U.S./" "$DST/U.S. Geology"
+ln -s "$SRC/pictures-3/Geology, US/" "$DST/U.S. Geology"
 ln -s "$SRC/pictures-4/0001/" "$DST/U.S. Cities"
 
 build $LOCAL $REMOTE
